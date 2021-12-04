e2911adf5f90c8b9908f8f51111e636514916d2c
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-12-04 10:24:59 +0600

    Add endpoints

--- src/App.js
+++ src/App.js
@@ -1,4 +1,84 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const URL = "https://api.themoviedb.org/3";
+const API_KEY = process.env.REACT_APP_TOKEN;
+
+const endpoints = {
+  originals: "/discover/tv",
+  trending: "/trending/all/week",
+  now_playing: "/movie/now_playing",
+  popular: "/movie/popular",
+  top_rated: "/movie/top_rated",
+  upcoming: "/movie/upcoming",
+};
+
 function App() {
+  const [originals, setOriginals] = useState([]);
+  const [trending, setTrending] = useState([]);
+  const [nowPlaying, setNowPlaying] = useState([]);
+  const [popular, setPopular] = useState([]);
+  const [topRated, setTopRated] = useState([]);
+  const [upcoming, setUpcoming] = useState([]);
+
+  useEffect(() => {
+    // Load Originals
+    axios
+      .get(`${URL}${endpoints.originals}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => setOriginals(res.data.results));
+
+    // Load Trending
+    axios
+      .get(`${URL}${endpoints.trending}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => setTrending(res.data.results));
+
+    // Load Now Playing
+    axios
+      .get(`${URL}${endpoints.now_playing}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => setNowPlaying(res.data.results));
+
+    // Load Popular
+    axios
+      .get(`${URL}${endpoints.popular}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => setPopular(res.data.results));
+
+    // Load Top Rated
+    axios
+      .get(`${URL}${endpoints.top_rated}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => setTopRated(res.data.results));
+
+    // Load Upcoming
+    axios
+      .get(`${URL}${endpoints.upcoming}`, {
+        params: {
+          api_key: API_KEY,
+        },
+      })
+      .then((res) => setUpcoming(res.data.results));
+
+    // Get other categories with the same pattern here
+  }, []);
+
   return (
     <>
       <h1>Hello world!</h1>
