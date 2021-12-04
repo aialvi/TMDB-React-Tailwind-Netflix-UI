d048dea25eeb1565d65aa087d35e8abd89659dac
Aminul Islam Alvi <aminulialvi@gmail.com>
2021-12-04 10:35:20 +0600

    Show movie originals posters and add movie poster style

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,9 +1,23 @@
-import { MoviesContainer, MoviesTitle } from "./Movies.styles";
+import {
+  MoviesContainer,
+  MoviesTitle,
+  MoviesRow,
+  MoviesPoster,
+} from "./Movies.styles";
 
 function Movies({ title, movies }) {
   return (
     <MoviesContainer>
       <MoviesTitle>{title}</MoviesTitle>
+      <MoviesRow>
+        {movies.map((movie) => (
+          <MoviesPoster
+            key={movie.id}
+            src={"https://image.tmdb.org/t/p/w300" + movie.poster_path}
+            alt={movie.name}
+          />
+        ))}
+      </MoviesRow>
     </MoviesContainer>
   );
 }
--- src/components/Movies.styles.js
+++ src/components/Movies.styles.js
@@ -14,3 +14,29 @@ export const MoviesTitle = styled.h2`
       mx-8
     `}
 `;
+
+export const MoviesRow = styled.div`
+  ${tw`
+      flex
+      overflow-x-auto
+      mt-4
+      p-4
+    `}
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
+
+export const MoviesPoster = styled.img`
+  ${tw`
+    m-2
+    w-40
+  `}
+
+  // Scale the movie img when the user hovers on it.
+  transition: all 0.2s;
+  &:hover {
+    transform: scale(1.09);
+  }
+`;
